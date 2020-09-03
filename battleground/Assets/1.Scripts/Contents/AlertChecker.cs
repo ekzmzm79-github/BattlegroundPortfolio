fbc16e3d34fa0041d7aea7886738193d0de804f2
ekzmzm79-github <57693531+ekzmzm79-github@users.noreply.github.com>
2020-09-03 18:12:10 +0900

    플레이어 컴포넌트 - HealthBase, AlertChecker
    PlayerHealth, AnemeyHealth class가 상속받는 기본 클래스 HealthBase 정의
    사격이 일어나면 Physics.OverlapSphere 기능을 통해 일정 반경의 콜라이더들에게 alert하여서 AlertNearBy 메소드 중첩적으로 실행
    -> 결과적으로 플레이어가 사격하면 인근의 적들이 반응해서 달려오는 기능 구현

---
+++ battleground/Assets/1.Scripts/Contents/AlertChecker.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertChecker : MonoBehaviour
+{
+    [Range(0,50)] public float alertRadius; // 알림 범위
+    public int extraWaves = 1; // 알림 횟수
+
+    public LayerMask alertMask = FC.TagAndLayer.LayerMasking.Enemy;
+    private Vector3 current;
+    private bool alert;
+
+    private void Start()
+    {
+        InvokeRepeating("PlngAlert", 1, 1);
+    }
+
+    private void AlertNearBy(Vector3 origin, Vector3 target, int wave =0)
+    {
+        if(wave > this.extraWaves)
+        {
+            return;
+        }
+        /*
+         * 중점과 반지름으로 가상의 원을 만들어 
+         * 추출하려는 반경 이내에 들어와 있는 콜라이더들을 반환하는 함수
+         */
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(origin, alertRadius, alertMask);
+        foreach(Collider collider in targetsInViewRadius)
+        {
+            /*
+             * SendMessage : 해당 오브젝트의 모든 컴포넌트에서 해당 이름의 함수를 모두 호출
+             * SendMessageUpwards : SendMessage + 모든 부모에게서도 검색해서 호출
+             * BroadcastMessage : SendMessage + 모든 자손들까지 전부 검색해서 호출
+             */
+            collider.SendMessageUpwards("AlertCallback", target, SendMessageOptions.DontRequireReceiver);
+            AlertNearBy(collider.transform.position, target, wave + 1); // 알림을 받은 적도 주변 적에게 알림 (wave + 1)
+        }
+    }
+
+    public void RootAlertNearBy(Vector3 origin)
+    {
+        current = origin;
+        alert = true;
+    }
+
+    void PlngAlert()
+    {
+        if(alert)
+        {
+            alert = false;
+            AlertNearBy(current, current);
+        }
+    }
+}
---
+++ battleground/Assets/1.Scripts/Contents/HealthBase.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// PlayerHealth, AnemeyHealth class가 상속받는 기본 클래스
+/// </summary>
+public class HealthBase : MonoBehaviour
+{
+    public class DamageInfo
+    {
+        #region Variable
+        public Vector3 location, direction; // 피격 위치와 방향
+        public float damage;
+        public Collider bodyPart; // 특정 파트에 대한 특수 데미지 처리 예비용
+        public GameObject origin; // 피격 이펙트 생성용 오브젝트
+
+        public DamageInfo(Vector3 location, Vector3 direction, float damage, Collider bodyPart = null, GameObject origin = null)
+        {
+            this.location = location;
+            this.direction = direction;
+            this.damage = damage;
+            this.bodyPart = bodyPart;
+            this.origin = origin;
+        }
+
+        #endregion Variable
+    }
+
+    //[HideInInspector]  : public 이지만 인스펙터에 노출되지 않음
+    [HideInInspector] public bool IsDead;
+    protected Animator myAnimator;
+
+    public virtual void TakeDamage(Vector3 location, Vector3 direction, float damage, Collider bodyPart = null, GameObject origin = null)
+    {
+
+    }
+
+
+    public void HitCallBack(DamageInfo damageInfo)
+    {
+        this.TakeDamage(damageInfo.location, damageInfo.direction, damageInfo.damage, damageInfo.bodyPart, damageInfo.origin);
+    }
+}
