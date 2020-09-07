ab5a7482d7e1a2b8319a2e6057e835a821069ebd
ekzmzm79-github <57693531+ekzmzm79-github@users.noreply.github.com>
2020-09-07 20:02:45 +0900

    플레이어 생멱력 및 발소리 컴퍼넌트
    PlayerHealth 스크립트와
    PlayerFootStep 스크립트 제작
    
    플레이어의 현재 체력 및 원래 체력 비율에 따라 Lerf하게 HealthBar가 작아지도록 구현할 예정
    
    플레이어의 LeftFoot, RightFoot의 현재 위치 및 oldDist, maxDist의 현재 값을 체크하여서 '땅 위에 있고 이동 중 이며 두 발의 위치가 같을 때 == 걷는중' 이라고 판단하여서 발소리 인덱스를 랜덤하게 결정하고 재생

---
+++ battleground/Assets/1.Scripts/Player/PlayerFootStep.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 발자국 소리 출력 
+/// </summary>
+public class PlayerFootStep : MonoBehaviour
+{
+    #region Variable
+    
+    public SoundList[] stepSounds;
+    private Animator myAnimator;
+    private int index;
+    private Transform leftFoot, rightFoot;
+    private float dist;
+    private int groundedBool, coverBool, aimBool, crouchFloat; //animator value
+    private bool grounded;
+
+    public enum Foot
+    {
+        LEFT,
+        RIGHT,
+    }
+    private Foot step = Foot.LEFT;
+    private float oldDist, maxDist = 0f; // 걷는 동안에 몇 프레임은 두 dist 값이 같다
+
+    #endregion Variable
+
+    #region Method
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+        leftFoot = myAnimator.GetBoneTransform(HumanBodyBones.LeftFoot);
+        rightFoot = myAnimator.GetBoneTransform(HumanBodyBones.RightFoot);
+        groundedBool = Animator.StringToHash(FC.AnimatorKey.Grounded);
+        coverBool = Animator.StringToHash(FC.AnimatorKey.Cover);
+        aimBool = Animator.StringToHash(FC.AnimatorKey.Aim);
+        crouchFloat = Animator.StringToHash(FC.AnimatorKey.Crouch);
+    }
+
+    private void PlayFootStep()
+    {
+        if(oldDist < maxDist)
+        {
+            return;
+        }
+
+        oldDist = maxDist = 0;
+        int oldIndex = index;
+        while(oldIndex == index)
+        {
+            //index를 랜덤하게
+            index = Random.Range(0, stepSounds.Length);
+        }
+
+        //랜덤한 stepSounds[index] 재생
+        SoundManager.Instance.PlayOneShotEffect((int)stepSounds[index], transform.position, 0.2f);
+    }
+
+    private void Update()
+    {
+        if(!grounded && myAnimator.GetBool(groundedBool))
+        {
+            PlayFootStep();
+        }
+
+        grounded = myAnimator.GetBool(groundedBool);
+
+        float factor = 0.15f; // 임의의 거리값 -> 이거보다 작다면 footSound 재생
+        if(grounded && myAnimator.velocity.magnitude > 1.6f) // 땅에 있고 움직이고 있다면
+        {
+            oldDist = maxDist;
+            switch(step)
+            {
+                case Foot.LEFT:
+                    //transform.position 은 발다닥을 기준으로 한다
+                    dist = leftFoot.position.y - transform.position.y;
+                    maxDist = dist > maxDist ? dist : maxDist;
+                    if(dist <= factor)
+                    {
+                        PlayFootStep();
+                        step = Foot.RIGHT;
+                    }
+                    break;
+                case Foot.RIGHT:
+                    dist = rightFoot.position.y - transform.position.y;
+                    maxDist = dist > maxDist ? dist : maxDist;
+                    if (dist <= factor)
+                    {
+                        PlayFootStep();
+                        step = Foot.LEFT;
+                    }
+                    break;
+            }
+        }
+    }
+
+    #endregion Method
+
+}
---
+++ battleground/Assets/1.Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 플레이어의 생명력을 담당
+/// 피격시 피격 이펙트를 표시하며 UI를 업데이트
+/// 죽을 경우 모든 동작 스크립트를 멈추게 만든다.
+/// </summary>
+public class PlayerHealth : HealthBase
+{
+    #region Variable
+    public float health = 100f;
+    public float criticalHealth = 30f;
+    public Transform healthHUD;
+    public SoundList deathSound;
+    public SoundList hitSound;
+    public GameObject hurtPrefab;
+    public float decayFactor = 0.8f; // 감쇄?
+
+    private float totalHealth;
+    private RectTransform healthBar, placeHolderBar;
+    private Text healthLabel;
+    private float originalBarScale;
+    private bool critical;
+
+    #endregion Variable
+
+    #region Method
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+        totalHealth = health;
+
+        healthBar = healthHUD.Find("HealthBar/Bar").GetComponent<RectTransform>();
+        placeHolderBar = healthHUD.Find("HealthBar/Placeholder").GetComponent<RectTransform>();
+        healthLabel = healthHUD.Find("HealthBar/Label").GetComponent<Text>();
+        originalBarScale = healthBar.sizeDelta.x;
+        healthLabel.text = "" + (int)health;
+ 
+    }
+
+    private void Update()
+    {
+        //hpBar가 서서히 줄어들도록 Lerp 
+        if (placeHolderBar.sizeDelta.x > healthBar.sizeDelta.x)
+        {
+            placeHolderBar.sizeDelta = Vector2.Lerp(placeHolderBar.sizeDelta, healthBar.sizeDelta, 2f * Time.deltaTime);
+        }
+    }
+
+    public bool IsFullLife()
+    {
+        return Mathf.Abs(health - totalHealth) < float.Epsilon;
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthLabel.text = "" + (int)health;
+        float scaleFactor = health / totalHealth;
+        healthBar.sizeDelta = new Vector2(scaleFactor * originalBarScale, healthBar.sizeDelta.y);
+    }
+
+    private void Kill()
+    {
+        IsDead = true;
+        gameObject.layer = FC.TagAndLayer.GetLayerByName(FC.TagAndLayer.LayerName.Default);
+        gameObject.tag = FC.TagAndLayer.TagName.Untagged;
+        healthHUD.gameObject.SetActive(false);
+        healthHUD.parent.Find("WeaponHUD").gameObject.SetActive(false);
+        myAnimator.SetBool(FC.AnimatorKey.Aim, false);
+        myAnimator.SetBool(FC.AnimatorKey.Cover, false);
+        myAnimator.SetFloat(FC.AnimatorKey.Speed, 0);
+
+        
+        foreach (GenericBehaviour behaviour in GetComponents<GenericBehaviour>()) // 원래는 GetComponentsInChildren
+        {
+            behaviour.enabled = false;
+        }
+        
+        SoundManager.Instance.PlayOneShotEffect((int)deathSound, transform.position, 5f);
+    }
+
+    public override void TakeDamage(Vector3 location, Vector3 direction, float damage, Collider bodyPart = null, GameObject origin = null)
+    {
+        health -= damage;
+        UpdateHealthBar();
+        if (health <= 0)
+        {
+            Kill();
+        }
+        else if(health <= criticalHealth && !critical)
+        {
+            critical = true;
+        }
+
+        SoundManager.Instance.PlayOneShotEffect((int)hitSound, location, 1f);
+    }
+
+    #endregion Method
+
+}
