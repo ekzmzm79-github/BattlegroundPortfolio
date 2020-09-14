e00985a53dcdb23bec21b2b65f5f175e5cc76b45
ekzmzm79-github <57693531+ekzmzm79-github@users.noreply.github.com>
2020-09-14 13:25:58 +0900

    EnemyAI - FieldOfViewEditor
    Enemy 캐릭터의 시야각도와 범위 및 감지 범위(원)를 에디터에서 직접적으로 확인하기 위한 Enemy/Editor/FieldOfViewEditor.cs 제작
    또한 Enemy를 추가할때마다 레이어와 태그 값을 자동으로 할동해주기 위한 SetupTagLayer 작성함

---
+++ battleground/Assets/1.Scripts/Enemy/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(StateController))]
+public class FieldOfViewEditor : Editor
+{
+    /// <summary>
+    /// 각도(부채꼴 모양의 시야각도)를 벡터로
+    /// (?)
+    /// </summary>
+    Vector3 DirFromAngle(Transform transform, float angleInDegrees, bool angleIsGlobal)
+    {
+        if(!angleIsGlobal)
+        {
+            //global이 아니라면 local 값을 더해줌
+            angleInDegrees += transform.eulerAngles.y;
+        }
+
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0f,
+            Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+
+    private void OnSceneGUI()
+    {
+        StateController fov = target as StateController;
+        if(fov == null || fov.gameObject == null)
+        {
+            return;
+        }
+
+        Handles.color = Color.white;
+        //PerCeption Area(circle)
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360f,
+            fov.perceptionRadius);
+        //near
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360f,
+            fov.perceptionRadius * 0.5f);
+
+        // 부채꼴 양끝 방향 벡터 구하기
+        Vector3 viewAngleA = DirFromAngle(fov.transform, -fov.viewAngle / 2, false);
+        Vector3 viewAngleB = DirFromAngle(fov.transform, fov.viewAngle / 2, false);
+
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, viewAngleA, fov.viewAngle, fov.viewRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
+
+        Handles.color = Color.yellow;
+        if(fov.targetInSight && fov.personalTarget != Vector3.zero)
+        {
+            Handles.DrawLine(fov.enemyAnimation.gunMuzzle.position, fov.personalTarget);
+        }
+    }
+
+}
---
+++ battleground/Assets/1.Scripts/Enemy/Editor/SetupTagLayer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Enemy를 매번 로딩할때마다 Tag와 Layer를 자동으로 세팅해주는 기능
+/// </summary>
+public class SetupTagLayer : Editor
+{
+    [MenuItem("GameObject/Enemy AI/Setup Tag and Layers", false, 11)]
+    static void Init()
+    {
+        GameObject go = Selection.activeGameObject;
+        go.tag = FC.TagAndLayer.TagName.Enemy;
+        go.layer = LayerMask.NameToLayer("Enemy");
+        GameObject hips = go.GetComponent<Animator>().GetBoneTransform(HumanBodyBones.Hips).gameObject;
+        if(!hips.GetComponent<Collider>())
+        {
+            hips = hips.transform.GetChild(0).gameObject;
+        }
+        hips.layer = LayerMask.NameToLayer("Enemy");
+        go.GetComponentInChildren<SkinnedMeshRenderer>().gameObject.layer =
+            LayerMask.NameToLayer("Ignore Raycast");
+        foreach(Transform child in go.GetComponent<Animator>().GetBoneTransform(HumanBodyBones.RightHand))
+        {
+            Transform gunMuzzle = child.Find("muzzle");
+            if(gunMuzzle != null)
+            {
+                child.gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
+                foreach(Transform part in child)
+                {
+                    part.gameObject.layer = child.gameObject.layer;
+                }
+            }
+        }
+    }
+}
+
+[InitializeOnLoad]
+public class StartUp
+{
+    static StartUp()
+    {
+        if(LayerMask.NameToLayer("Enemy") != 12)
+        {
+            Debug.LogWarning("Enemy Layer Missing!");
+        }
+    }
+}
