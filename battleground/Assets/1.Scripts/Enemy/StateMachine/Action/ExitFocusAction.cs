4f103356a29f745aa74415d996a863e402eee2a5
ekzmzm79-github <57693531+ekzmzm79-github@users.noreply.github.com>
2020-09-22 11:22:01 +0900

    Enemy State Setting
    여태 작성했던 Decision, State를 통해서 각 State마다 Transition을 작성하여 세팅.
    이로서 Enemy AI에 대한 FSM이 완료됨

---
+++ battleground/Assets/1.Scripts/Enemy/StateMachine/Action/ExitFocusAction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 전투가 끝나서 모든 감지가 해제되는 액션
+/// </summary>
+[CreateAssetMenu(menuName = "PluggableAI/Actions/ExitFocus")]
+public class ExitFocusAction : Action
+{
+    public override void OnReadyAction(StateController controller)
+    {
+        controller.focusSight = false;
+        controller.variables.feelAlert = false;
+        controller.variables.hearAlert = false;
+        controller.Strafing = false;
+        controller.nav.destination = controller.personalTarget;
+        controller.nav.speed = 0f;
+    }
+
+    public override void Act(StateController controller)
+    {
+
+    }
+}
